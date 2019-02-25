92dfbfc2cc53dc9c8949bf0939e866b0edec0746
DESKTOP-01T3UKC\matth <mpru223@g.uky.edu>
2019-02-25 15:31:51 -0500

    started building adjacency matrix

--- lgf.m
+++ lgf.m
@@ -6,7 +6,7 @@ load datasets\Indian_pines.mat
 
 % reshape 145x145 image to column vector
 IndP=reshape(indian_pines,[21025 220]);
-XSpec=IndP;
+X_Spec=IndP';
 
 % do pca on the hs data, multiply to find pc, back to image sizes
 IndP=reshape(IndP*pca(IndP), [145 145 220]);
@@ -19,11 +19,11 @@ IndP=IndP(:,:,1:p);
 
 % var to hold final stackup
 % order: pca1,op1,op2,op3,clo1,clo2,clo3,pca2,op1,op2...
-XSpat=[]; 
+X_Spat=[]; 
 
 for i=1:size(IndP,3)
     I = IndP(:,:,i); % orig pca image
-    XSpat(:,:,(i-1)*(2*M+1)+1)=I;
+    X_Spat(:,:,(i-1)*(2*M+1)+1)=I;
     
     % 3 openings and 3 closings 
     m=2; % counter
@@ -32,13 +32,27 @@ for i=1:size(IndP,3)
         se=strel('diamond',nS);
         % perform an opening
         I=imopen(IndP(:,:,i),se);
-        XSpat(:,:,(i-1)*(2*M+1)+m)=I;
+        X_Spat(:,:,(i-1)*(2*M+1)+m)=I;
         % perform a closing
         I=imclose(IndP(:,:,i),se);
-        XSpat(:,:,(i-1)*(2*M+1)+m+3)=I;
+        X_Spat(:,:,(i-1)*(2*M+1)+m+3)=I;
         m=m+1;
     end
 end
-    
+
+% stack spectral and spatial features
+X_Spat=reshape(X_Spat,[21025 D])';
+X_Sta=[X_Spec;X_Spat];
+% test adjacency matrix
+A=zeros(size(X_Sta,2),size(X_Sta,2));
+
+
+
+for i=1:size(A,2)
+   for j=1:size(A,2)
+       A(i,j)=sqrt(X_Sta(1,i)^2 + X_Sta(1,j)^2);
+   end
+end
+
     
     
