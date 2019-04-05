7f604d04b3c1bfed651e0f1ffd71811ad44ed8ef
DESKTOP-01T3UKC\matth <mpru223@g.uky.edu>
2019-04-05 12:24:58 -0400

    work on blocklgf process

--- runBlock.m
+++ runBlock.m
@@ -6,4 +6,11 @@ load datasets/Indian_pines.mat
 % make subset of data for quickness of testing 
 x = indian_pines(1:32,1:32,:);
 
-A = blockProcessLGF(x, 5, 5);
+A = blockProcessLGF(x, 3, 5);
+
+x=reshape(A,[32 32 1024]);
+
+for i=1:1024
+    imagesc(x(:,:,i))
+    drawnow
+end
