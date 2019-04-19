6ab231eedf42367998fb3ea69beae303535153b7
Daniel L. Lau <dllau@engr.uky.edu>
2019-04-19 17:33:42 -0400

    Making use of Matlab's graph visualization.

--- runBlock.m
+++ runBlock.m
@@ -5,12 +5,23 @@ load datasets/Indian_pines.mat
 
 % make subset of data for quickness of testing 
 x = indian_pines(1:32,1:32,:);
+[N,M] = meshgrid([1:size(x,2)],[1:size(x,1)]);
 
 A = blockProcessLGF(x, 3, 5);
 
-x=reshape(A,[32 32 1024]);
+%%% A = graph weight matrix
+%%% M = row coordinate of graph nodes
+%%% N = col coordinate of graph nodes
+G = graph(A > 0, 'upper');
 
-for i=1:1024
-    imagesc(x(:,:,i))
-    drawnow
-end
+%%% now visualize the graph
+figure(1);
+plot(G);
+
+%%% visualize the connections between pixels
+figure(2);
+imagesc(x(:,:,1));
+colormap(gray());
+hold on;
+h = plot(G, 'XData', N(:),'YData', M(:));
+set(h, 'linewidth', 2);
